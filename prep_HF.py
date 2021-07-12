910d143430dd30cdf0010510293eec60e4d4ba46
Glenn Liu <glenn.y.liu@gmail.com>
2021-07-12 16:01:50 -0400

    Temp fix for file endings
    extra .npz in file names, need to fix

--- prep_HF.py
+++ prep_HF.py
@@ -88,9 +88,9 @@ def calc_corrthres(p,tails,dof):
 #%%
 
 # Load in the damping and correlation files
-damping = np.load("%s%s_Damping_%s.npy"% (datpath,vname,expin))  # [mon lag lat lon] 
-rsst = np.load("%sSST_Autocorrelation_%s.npy"% (datpath,expin))  # [mon lag lat lon] 
-rflx = np.load("%s%s_Crosscorrelation_%s.npy"% (datpath,vname,expin))  # [mon lag lat lon] 
+damping = np.load("%s%s_Damping_%s.npz.npy"% (datpath,vname,expin))  # [mon lag lat lon] 
+rsst = np.load("%sSST_Autocorrelation_%s.npz.npy"% (datpath,expin))  # [mon lag lat lon] 
+rflx = np.load("%s%s_Crosscorrelation_%s.npz.npy"% (datpath,vname,expin))  # [mon lag lat lon] 
 
 # Load land ice mask and lat/lon
 limask = np.load(lipath) # [lat x lon]
