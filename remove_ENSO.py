cad630f93f00b374e89f6f92c3559181e8208829
Glenn <glenn.y.liu@gmail.com>
2022-08-05 11:05:55 -0400

    Added option to keep ENSO
    - Added option to not remove ENSO. (Essentially consolidates PIC simulation files into the same format as if ENSO was removed, also calculates monthly anomalies via removal of the seasonal cycle)
    - Modified file structure so that FULL and SLAB go into their respective files
    - Changed naming format for ENSO non-removal cases. Probably need to do some work to generalize this.  ENSO removal cases need to be more verbose since the include the various lag and Pc information.

--- remove_ENSO.py
+++ remove_ENSO.py
@@ -14,31 +14,36 @@ import xarray as xr
 import numpy as np
 import glob
 import time
-
+import sys
 #%% User Edits
 
+# Currently only runs on stormtrack
+sys.path.append("/home/glliu/00_Scripts/01_Projects/00_Commons/")
+from amv import proc,viz
+
 # Paths
-datpath = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/01_hfdamping/hfdamping_PIC_SLAB/ENSO/" # Path to ENSO Data
-varpath = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/downloaded/" # Path to raw variables
-outpath = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/01_hfdamping/hfdamping_PIC_SLAB/02_ENSOREM/" # Output Path
+datpath   = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/01_hfdamping/hfdamping_PIC_SLAB/ENSO/" # Path to ENSO Data
+varpath   = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/downloaded/" # Path to raw variables
+outpath   = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/01_hfdamping/hfdamping_PIC_SLAB/02_ENSOREM/" # Output Path
 
 # Name of the ENSO file
-mconfig  = "FULL"
-ensofile = "EOF_ENSO_PIC_%s.npz" % mconfig # Name of the file containing ENSO indices (.npz)
-ensoname = "pcs" # Name of the ENSO Index (PC) variable
+mconfig   = "SLAB"
+ensofile  = "EOF_ENSO_PIC_%s.npz" % mconfig # Name of the file containing ENSO indices (.npz)
+ensoname  = "pcs" # Name of the ENSO Index (PC) variable
 
 # Variables to process
-vnames = ["SHFLX","LHFLX","FSNS","FLNS"]#["TS"]#,"SHFLX","LHFLX","FSNS","FLNS"]
+vnames    = ["SHFLX","LHFLX","FSNS","FLNS"]#["TS"]#,"SHFLX","LHFLX","FSNS","FLNS"]
 
 # Removal choices
+ensorem   = False # Set to True to remove enso, False to save with enso present (just consolidating files)
 pcrem     = 2 # Number of EOFs to remove
 ensolag   = 1 # Lag between Variable and ENSO (Variable lags ENSO by _ months)
 monwin    = 3 # Moving window of months to consider (3 months centered around 1)
 savepat   = True # Set to true to save ENSO patterns
 reduceyr  = True # Reduce time-period to account of lags and year crossings 
 
-if mconfig =="FULL":
-    outpath = outpath + '/FULL/'
+# if mconfig =="FULL":
+#     outpath = outpath + '/FULL/'
 
 
 #%% Functions
@@ -95,27 +100,34 @@ def indexwindow(invar,m,monwin,combinetime=False):
 #%% Load in the data
 allstart = time.time()
 
+outpath += "%s/" % mconfig
+
 # Load ENSO
-ld = np.load(datpath+ensofile,allow_pickle=True)
-ensoid = ld[ensoname] # [year x  month x pc]
+if ensorem:
+    ld     = np.load(datpath+ensofile,allow_pickle=True)
+    ensoid = ld[ensoname] # [year x  month x pc]
+    
+    # Standardize the index (stdev = 1)
+    ensoid = ensoid/np.std(ensoid,(0,1))
 
-# Standardize the index (stdev = 1)
-ensoid = ensoid/np.std(ensoid,(0,1))
-
-# Shift ENSO by the specified enso lag
-ensoid = np.roll(ensoid,ensolag,axis=1) # (ex: Idx 0 (Jan) is now 11)
-
+    # Shift ENSO by the specified enso lag
+    ensoid = np.roll(ensoid,ensolag,axis=1) # (ex: Idx 0 (Jan) is now 11)
+    
 # Reduce years if needed
 if reduceyr: # Since enso leads, drop years from end of period
     dropyr = int(np.fix(ensolag/12) + 1)
-    ensoid=ensoid[:-dropyr,:,:]
+    
+    if ensorem: # Drop years for index if removal is part of the step
+        ensoid=ensoid[:-dropyr,:,:] 
 
 
 for v,vname in enumerate(vnames):
-       
-        
+    
     # Set experiment name
-    outname = "ENSOREM_%s_lag%i_pcs%i_monwin%i" %(vname,ensolag,pcrem,monwin)
+    if ensorem:
+        outname = "ENSOREM_%s_lag%i_pcs%i_monwin%i" %(vname,ensolag,pcrem,monwin)
+    else:
+        outname = "ENSOREM0_%s_lag%i" % (vname,ensolag)
         
     # Set path
     ncpath = '%satm/proc/tseries/monthly/%s/' % (varpath,vname)
@@ -123,9 +135,9 @@ for v,vname in enumerate(vnames):
         ncsearch = 'e.e11.E1850C5CN.f09_g16.001.cam.h0.%s.*.nc' % vname
     else:
         ncsearch = 'b.e11.B1850C5CN.f09_g16.005.cam.h0.%s.*.nc' % vname
+    
     nclist = glob.glob(ncpath+ncsearch)
     nclist.sort()
-    
     
     if len(nclist)<1:
         print("WARNING NO FILES FOUND!")
@@ -149,7 +161,7 @@ for v,vname in enumerate(vnames):
     
     # Open dataset # [time x lat x lon]
     st = time.time()
-    dsall = xr.open_mfdataset(nclist,concat_dim='time',preprocess=preprocess)
+    dsall = xr.open_mfdataset(nclist,concat_dim='time',preprocess=preprocess,combine='nested')
     #print("Opened in %.2fs"%(time.time()-st))
     
     # Read out the variables (bottleneck step here) [time x lat x lon]
@@ -169,71 +181,83 @@ for v,vname in enumerate(vnames):
         vanom=vanom[dropyr:,:,:,:]
     nyr,nmon,nlat,nlon = vanom.shape
     
-    # Reshape to combine spatial dimensions
-    vanom  = vanom.reshape(nyr,nmon,nlat*nlon) # [year x mon x space]
-    vout   = vanom.copy()
-    
-    # Variable to save enso pattern
-    ensopattern = np.zeros((12,nlat,nlon,pcrem))
-    
-    # Looping by PC...
-    for pc in range(pcrem):
+    if ensorem:
+        # Reshape to combine spatial dimensions
+        vanom  = vanom.reshape(nyr,nmon,nlat*nlon) # [year x mon x space]
+        vout   = vanom.copy()
         
-        # Looping for each month
-        for m in range(12):
-            
-            # Set up indexing
-            if monwin > 1:  
-                winsize = int(np.floor((monwin-1)/2))
-                monid = [m-winsize,m,m+winsize]
-                if monid[2] > 11: # Reduce end month if needed
-                    monid[2] -= 12
-            else:
-                winsize = 0
-                monid = [m]
-                
-             
-            if reduceyr:
-                ensoin = indexwindow(ensoid[:,:,[pc]],m,monwin,combinetime=True).squeeze()
-                varin = indexwindow(vanom,m,monwin,combinetime=True)
-                nyr   = int(ensoin.shape[0]/monwin) # Get new year dimension
-            else:
-                # Index corresponding timeseries
-                ensoin = ensoid[:,monid,pc] # [yr * mon]  Lagged ENSO
-                varin = vanom[:,monid,:] # [yr * mon * space] Variable
-                
-                # Re-combine time dimensions
-                ensoin = ensoin.reshape(nyr*monwin)
-                varin = varin.reshape(nyr*monwin,nlat*nlon)
-            
-            # Regress to obtain coefficients [space]
-            varreg,_ = proc.regress_2d(ensoin.squeeze(),varin,nanwarn=1)
-            
-            # Write to enso pattern
-            ensopattern[m,:,:,pc] = varreg.reshape(nlat,nlon).copy()
-            
-            # Expand and multiply out and take mean for period [space,None]*[None,time] t0o [space x time]
-            ensocomp = (varreg[:,None] * ensoin[None,:]).squeeze()
-            
-            # Separate year and mon and take mean along months
-            ensocomp = ensocomp.reshape(nlat*nlon,nyr,monwin).mean(2)
-            
-            # Remove the enso component for the specified month
-            vout[winsize:-winsize,m,:] -= ensocomp.T
-            
-            print("Removed ENSO Component for %s: PC %02i | Month %02i (t=%.2fs)" % (vname,pc+1,m+1,time.time()-allstart))
-            # < End Mon Loop>
-        # < End PC Loop>
+        # Variable to save enso pattern
+        ensopattern = np.zeros((12,nlat,nlon,pcrem))
         
-    # Save output variables
-    np.savez("%s%s.npz"%(outpath,outname),**{
-        vname:vout[winsize:-winsize,:,:].reshape(nyr*12,nlat,nlon),
-        'lon':lon,
-        'lat':lat}
-             )
-    np.savez("%s%s_ensocomponent.npz"%(outpath,outname),**{
-        'ensopattern':ensopattern,
-        'lon':lon,
-        'lat':lat}
-             )
+        # Looping by PC...
+        for pc in range(pcrem):
+            
+            # Looping for each month
+            for m in range(12):
+                
+                # Set up indexing
+                if monwin > 1:  
+                    winsize = int(np.floor((monwin-1)/2))
+                    monid = [m-winsize,m,m+winsize]
+                    if monid[2] > 11: # Reduce end month if needed
+                        monid[2] -= 12
+                else:
+                    winsize = 0
+                    monid = [m]
+                    
+                if reduceyr:
+                    ensoin = indexwindow(ensoid[:,:,[pc]],m,monwin,combinetime=True).squeeze()
+                    varin = indexwindow(vanom,m,monwin,combinetime=True)
+                    nyr   = int(ensoin.shape[0]/monwin) # Get new year dimension
+                else:
+                    # Index corresponding timeseries
+                    ensoin = ensoid[:,monid,pc] # [yr * mon]  Lagged ENSO
+                    varin = vanom[:,monid,:] # [yr * mon * space] Variable
+                    
+                    # Re-combine time dimensions
+                    ensoin = ensoin.reshape(nyr*monwin)
+                    varin = varin.reshape(nyr*monwin,nlat*nlon)
+                
+                # Regress to obtain coefficients [space]
+                varreg,_ = proc.regress_2d(ensoin.squeeze(),varin,nanwarn=1)
+                
+                # Write to enso pattern
+                ensopattern[m,:,:,pc] = varreg.reshape(nlat,nlon).copy()
+                
+                # Expand and multiply out and take mean for period [space,None]*[None,time] t0o [space x time]
+                ensocomp = (varreg[:,None] * ensoin[None,:]).squeeze()
+                
+                # Separate year and mon and take mean along months
+                ensocomp = ensocomp.reshape(nlat*nlon,nyr,monwin).mean(2)
+                
+                # Remove the enso component for the specified month
+                vout[winsize:-winsize,m,:] -= ensocomp.T
+                
+                print("Removed ENSO Component for %s: PC %02i | Month %02i (t=%.2fs)" % (vname,pc+1,m+1,time.time()-allstart))
+                # < End Mon Loop>
+            # < End PC Loop>
+        
+        np.savez("%s%s.npz"%(outpath,outname),**{
+            vname:vout[winsize:-winsize,:,:].reshape(nyr*12,nlat,nlon),
+            'lon':lon,
+            'lat':lat}
+                 )
+        np.savez("%s%s_ensocomponent.npz"%(outpath,outname),**{
+            'ensopattern':ensopattern,
+            'lon':lon,
+            'lat':lat}
+                 )
+    else: # Otherwise just consolidate the save the variable
+        # if monwin > 1:  
+        #     winsize = int(np.floor((monwin-1)/2))
+        # else:
+        #     winsize=0
+        np.savez("%s%s.npz"%(outpath,outname),**{
+            vname: vanom[:,:,:].reshape(nyr*12,nlat,nlon),
+            #vname:vanom[winsize:-winsize,:,:].reshape(nyr*12,nlat,nlon),
+            'lon':lon,
+            'lat':lat}
+                 )
+    # End ENSOREM conditional
+        
     print("Completed variable %s (t=%.2fs)" % (vname,time.time()-allstart))
