1eb6360d2401fc504048898fa04ddd6d976c5a3e
Glenn Liu <glenn.y.liu@gmail.com>
2024-02-01 14:02:01 -0500

    Start new file for prepping HFF inputs for stochastic model

---
+++ Main/make_HF_input.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+-----------------------------
+Make Heat Flux Feedback Input
+-----------------------------
+
+Takes a given input (from [combine_damping_CESM1LE.py], or [calc_enso_general], etc)
+Applies significance testing, and generates output ready for input into the stochastic model
+
+Relies on parameters indicated in [stochmod_params] (or maybe generate them here and copy em over)
+Trying to generalize function in [prep_HF]
+
+Created on Thu Feb  1 13:38:30 2024
+@author: gliu
+
+"""
+
+#%%
+
+#%%
+
