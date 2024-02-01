a831273e1f7acac63bfe8ec8492340ec3667c5ff
Glenn Liu <glenn.y.liu@gmail.com>
2024-02-01 15:38:21 -0500

    Add Nomask Run

--- hfcalc_params.py
+++ hfcalc_params.py
@@ -52,22 +52,22 @@ hparam2 = {
     'method'  : 4       # Significance test option: 1 (No Mask); 2 (SST autocorr); 3 (SST-FLX crosscorr); 4 (Both), 5 (Replace with SLAB values)
     }
 
-hname2  = "htr4" # Same as default but with lower significance rate due to reduced DOF in historical
-hparam2 = {
+hname3  = "nomasklag1" # Same as default but with lower significance rate due to reduced DOF in historical
+hparam3 = {
     'ensorem' : 1,      # 1=enso removed, 0=not removed
     'ensolag' : 1,      # Lag Applied toENSO and Variable before removal
     'monwin'  : 3,      # Size of month window for HFF calculations
     'detrend' : 1,      # Whether or not variable was detrended
     'tails'   : 2,      # tails for t-test
-    'p'       : 0.20,   # p-value for significance testing
+    'p'       : 1.00,   # p-value for significance testing
     'sellags' : [0,],   # Lags included (indices, so 0=lag1)
     'lagstr'  : "lag1", # Name of lag based on sellags
-    'method'  : 4       # Significance test option: 1 (No Mask); 2 (SST autocorr); 3 (SST-FLX crosscorr); 4 (Both), 5 (Replace with SLAB values)
+    'method'  : 1       # Significance test option: 1 (No Mask); 2 (SST autocorr); 3 (SST-FLX crosscorr); 4 (Both), 5 (Replace with SLAB values)
     }
 
 # Combine and make the dictionaries
-hff_names = [hname1,hname2,]
-hff_dicts = [hparam1,hparam2,]
+hff_names = [hname1,hname2,hname3,]
+hff_dicts = [hparam1,hparam2,hparam3,]
 hff_sets  = dict(zip(hff_names,hff_dicts))
 
 #%%
