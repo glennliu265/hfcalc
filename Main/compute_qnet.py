16863aded8165cb89092c434dc6291d7077dcb60
Glenn Liu <glenn.y.liu@gmail.com>
2024-07-02 14:33:01 -0400

    Add support for cesm1_htr_5deg

--- Main/compute_qnet.py
+++ Main/compute_qnet.py
@@ -23,8 +23,8 @@ import matplotlib.pyplot as plt
 #%% User Edits
 # ------------
 
-procname = "cesm2_pic_regioncut"
-
+#procname = "cesm2_pic_anom"#"cesm2_pic_regioncut"
+procname = "cesm1_htr_5degbilinear"
 
 #"cesm2_pic_anom"
 
@@ -32,7 +32,7 @@ procname = "cesm2_pic_regioncut"
 if procname == "cesm2_pic_anom":
     # Dataset and path information
     datname     = "cesm2_pic" # Name of dataset (for input and output)
-    datpath     = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/01_hfdamping/output/anom/"
+    datpath     = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/01_hfdamping/output/proc/"
     outpath     = datpath # Output Path
     vname_out   = "qnet" # 
     outname     = "%s%s_%s_anom.nc" % (datpath,datname,vname_out)
@@ -65,7 +65,28 @@ elif procname == "cesm2_pic_regioncut":
     nclist          = [nc_fsns,nc_flns,nc_shflx,nc_lhflx]
     vnames          = ["FSNS","FLNS","SHFLX","LHFLX"] # Shortwave first! Names of variables in netCDFs
     
-
+# Version (3), Global data (coarsened) from CESM1
+elif procname == "cesm1_htr_5degbilinear":
+    
+    # Dataset and path information
+    datname     = procname # Name of dataset (for input and output)
+    datpath     = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/01_hfdamping/output/proc/"
+    outpath     = datpath # Output Path
+    vname_out   = "qnet" # 
+    
+    outname     = "%s%s_%s_Global_1920to2005.nc" % (datpath,datname,vname_out)
+    print("File will be saved as %s" % outname)
+    
+    # NC files (can be anomalized, assume **Positive Upwards**, all units [W/m2])
+    nc_fsns         = datpath + "cesm1_htr_5degbilinear_FSNS_Global_1920to2005.nc"  # Incoming Shortwave
+    nc_flns         = datpath + "cesm1_htr_5degbilinear_FLNS_Global_1920to2005.nc"  # Outgoing Longwave
+    nc_shflx        = datpath + "cesm1_htr_5degbilinear_SHFLX_Global_1920to2005.nc" # Outgoing Sensible Heat Flux
+    nc_lhflx        = datpath + "cesm1_htr_5degbilinear_LHFLX_Global_1920to2005.nc" # Outgoing Latent Heat Flux
+    nclist          = [nc_fsns,nc_flns,nc_shflx,nc_lhflx]
+    vnames          = ["FSNS","FLNS","SHFLX","LHFLX"] # Shortwave first! Names of variables in netCDFs
+ 
+#_Global_1920to2005
+    
 
 #%%
 # Glossary of names to check (later, add CESM1/CESM2, CMIP5, and CMIP6 definitions, etc)
@@ -86,6 +107,8 @@ qnet_names      = ["NHFLX","QNET","qnet",'nhflx']
 #%% Add Everything Together
 # -------------------------
 
+    
+
 lonf  = 330
 latf  = 50
 check = []
@@ -98,6 +121,14 @@ for vv in range(4):
     ds    = xr.open_dataset(nclist[vv])[vname].load() # Maybe add region crop before load...
     print("Loaded data in %.2fs" % (time.time()-st))
     
+        
+    # Set ensemble flag
+    lensflag = False
+    if 'ens' in list(ds.dims):
+        print("Ensemble dimension detected!")
+        lensflag = True
+        
+    
     if vname in fsns_names:
         ds      = ds * -1 # Flip to positive upwards
         print("Flipping sign for Incoming Shortwave")
@@ -109,7 +140,10 @@ for vv in range(4):
         ds_qnet = ds_qnet + ds
     
     # Check Append
-    check.append(ds.sel(lon=lonf,lat=latf,method='nearest').isel(time=1).values.item())
+    if lensflag:
+        check.append(ds.sel(lon=lonf,lat=latf,method='nearest').isel(time=1,ens=0).values.item())
+    else:
+        check.append(ds.sel(lon=lonf,lat=latf,method='nearest').isel(time=1).values.item())
     
     ds.close()
     print("Completed %s in %.2fs" % (vname,time.time()-st))
@@ -117,7 +151,10 @@ for vv in range(4):
 
 print("QNET = -FSNS + (FLNS + SHFLX + LHFLX)" )
 print("%.2f = %.2f + (%.2f +  %.2f +  %.2f)" % (np.array(check).sum(),check[0],check[1],check[2],check[3]))
-print("Calculated value = %.2f" % (ds_qnet.sel(lon=lonf,lat=latf,method='nearest').isel(time=1).values.item()))
+if lensflag:
+    print("Calculated value = %.2f" % (ds_qnet.sel(lon=lonf,lat=latf,method='nearest').isel(time=1,ens=0).values.item()))
+else:
+    print("Calculated value = %.2f" % (ds_qnet.sel(lon=lonf,lat=latf,method='nearest').isel(time=1).values.item()))
 
 #%% Save output
 
