77a358afe82c568c16a89d5c4ad63c2778308595
Glenn Liu <glenn.y.liu@gmail.com>
2024-07-08 13:56:08 -0400

    Add New damping Set

--- hfcalc_params.py
+++ hfcalc_params.py
@@ -91,9 +91,22 @@ hparam4 = {
     'method'  : 1       # Significance test option: 1 (No Mask); 2 (SST autocorr); 3 (SST-FLX crosscorr); 4 (Both), 5 (Replace with SLAB values)
     }
 
+hname5 = "cesm1le5degqnetDamp" # cesm2_pic damping (years 200-2000), lag 1
+hparam5 = {
+    'ensorem' : 1,      # 1=enso removed, 0=not removed
+    'ensolag' : 1,      # Lag Applied toENSO and Variable before removal
+    'monwin'  : 3,      # Size of month window for HFF calculations
+    'detrend' : 1,      # Whether or not variable was detrended
+    'tails'   : 2,      # tails for t-test
+    'p'       : 1.00,   # p-value for significance testing
+    'sellags' : [0,],   # Lags included (indices, so 0=lag1)
+    'lagstr'  : "lag1", # Name of lag based on sellags
+    'method'  : 1       # Significance test option: 1 (No Mask); 2 (SST autocorr); 3 (SST-FLX crosscorr); 4 (Both), 5 (Replace with SLAB values)
+    }
+
 # Combine and make the dictionaries
-hff_names = [hname1,hname2,hname3,hname4,]
-hff_dicts = [hparam1,hparam2,hparam3,hparam4,]
+hff_names = [hname1,hname2,hname3,hname4,hname5,]
+hff_dicts = [hparam1,hparam2,hparam3,hparam4,hname5]
 hff_sets  = dict(zip(hff_names,hff_dicts))
 
 #%%
