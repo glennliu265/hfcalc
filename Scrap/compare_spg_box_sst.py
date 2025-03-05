a62f8fb0bde81122a69efc5312cdea7573f24642
Glenn Liu <glenn.y.liu@gmail.com>
2025-03-05 15:16:14 -0500

    Add MLD plots, cropping option, stochastic model
    - Add option to include stochastic model ACF
    - Plot the actual timeseries

--- Scrap/compare_spg_box_sst.py
+++ Scrap/compare_spg_box_sst.py
@@ -71,8 +71,10 @@ def preproc_ds(ds_in,bbin):
     
     # Detrend (simple linear)
     spg_dt   = sp.signal.detrend(spg_anom,0,type='linear')#proc.xrdetrend(spg_anom,timename='time',verbose=False)
+    
     indict   = {'time':ds_in.time}
     spg_dt   = xr.DataArray(spg_dt,dims=indict,coords=indict)
+    
     return spg_dt
 
 def calc_acf_mon(sst,lags):
@@ -94,6 +96,13 @@ def get_year_range(ds):
     yend   = str(times[-1].data)[:4]
     ystr   = "%s to %s" % (ystart,yend)
     return ystr
+    
+
+def crop_dataset(ds,ycrop):
+    tstart = "%04i-01-01" % ycrop[0]
+    tend   = "%04i-12-31" % ycrop[1]
+    dscrop = ds.sel(time=slice(tstart,tend))
+    return dscrop
 
 def winter_acf(ts):
     ts_wint = ds.where(ts.time.dt.month.isin([12,1,2,3]),drop=True)
@@ -114,17 +123,25 @@ def winter_acf(ts):
 #outpath     = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/SPG_Box/"
 
 dpath       = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/01_hfdamping/01_Data/spg_data/"
-dset_names  = ("OISST","HadISST","ERA20C","cesm1_pic","cesm2_pic")
-cols        = ("cornflowerblue",'salmon','goldenrod','gray','k')
+dset_names  = ("OISST","ERA5","HadISST","ERA20C","cesm1_pic","cesm2_pic")
+cols        = ("cornflowerblue","hotpink",'darkmagenta','forestgreen','gray','k')
+els         = ('solid','solid','dotted','dotted','dashed','dashed')
 
 #ystarts = ["1982","1950"]
+
+crop_ts = True
+ycrops      = ([1982,2020],[1982,2020],
+              [1980,2018],[1972,2010],
+              [2162,2200],[1962,2000])
 
 ndatasets   = len(dset_names)
 ncnames     = ["%s%s_SPG.nc" % (dpath,dset_names[nn]) for nn in range(ndatasets)]
 ds_all      = [xr.open_dataset(nc).load() for nc in ncnames]
+
+# Handle some time cropping
+if crop_ts:
+    ds_all  = [crop_dataset(ds_all[ii],ycrops[ii]) for ii in range(ndatasets)]
 ntimes      = [len(ds.time) for ds in ds_all]
-
-
 ystrs       = [get_year_range(ds) for ds in ds_all]
 
 lags  =  np.arange(61)
@@ -143,22 +160,42 @@ ds_masks = xr.open_dataset(nc_masks).load()
 
 # Load AVISO
 dpath_aviso = dpath_ice + "proc/"
-nc_adt = dpath_aviso + "AVISO_adt_NAtl_1993_2022_clim.nc"
-ds_adt = xr.open_dataset(nc_adt).load()
-cints_adt = np.arange(-100, 110, 10)
+nc_adt      = dpath_aviso + "AVISO_adt_NAtl_1993_2022_clim.nc"
+ds_adt      = xr.open_dataset(nc_adt).load()
+cints_adt   = np.arange(-100, 110, 10)
+
+
+# Load MIMOC
+dpath_mimoc = dpath_ice
+nc_mld      = dpath_ice + "MIMOC_RegridERA5_mld_NAtl_Climatology.nc"
+ds_mld      = xr.open_dataset(nc_mld).load()
+
+# Load Stochastic Model output from `single_point_run`
+nc_sm       = "stochastic_model_point.nc"
+ds_sm       = xr.open_dataset(dpath + nc_sm).load()
+
+
 
 #%% Preprocessing
 
+bbox_yeager = [-50,-10,50,60] # Original
+#bbox_yeager = [-30,-10,50,60] # Away from Sea Ice 
 
-
-bbox_yeager = [-50,-10,50,50] # Original
-bbox_yeager = [-30,-10,50,60] # Away from Sea Ice 
-
-bbfn,bbstr = proc.make_locstring_bbox(bbox_yeager)
+bbfn,bbstr  = proc.make_locstring_bbox(bbox_yeager)
 
 spg_anom    = [preproc_ds(ds.sst,bbox_yeager) for ds in ds_all]
-
 spg_acf     = [calc_acf_mon(sst.data,lags) for sst in spg_anom]
+
+#%% Compute Stochastic model acfs
+
+#ssts_sm         = ds_sm.sst
+ssts_sm_list    = [ds_sm.sst.isel(ens=ii) for ii in range(10)]
+
+if crop_ts:
+    ssts_sm_list = [ds.isel(time=slice(-(38*12)-1,-1)) for ds in ssts_sm_list]
+
+spg_acf_sm = [calc_acf_mon(sst.data,lags) for sst in ssts_sm_list]
+
 
 #%% Plot The ACF
 
@@ -173,9 +210,17 @@ ax.set_title(title,fontsize=16)
 for nn in range(ndatasets):
     label = "%s (Years %s)" % (dset_names[nn],ystrs[nn])
     ax.plot(lags,spg_acf[nn][kmonth,:],label=label,
-            c=cols[nn],linewidth=2.5)
-    
-ax.legend(fontsize=12)
+            c=cols[nn],linewidth=2.5,ls=els[nn])
+
+# Add Stochastic Model Plots
+for e in range(10):
+    if e == 0:
+        label="Stochastic Model"
+    else:
+        label=""
+    ax.plot(lags,spg_acf_sm[e][kmonth,:],label=label,c='goldenrod',alpha=0.5)
+
+#ax.legend(fontsize=12)
 
 figname = figpath + "ACF_Comparison_Obs_mon%02i.png" % (kmonth+1)
 
@@ -183,9 +228,7 @@ plt.savefig(figname,dpi=150,bbox_inches='tight',transparent=True)
 
 #%% Also Include a Locator
 
-imon=kmonth
-
-
+imon   = kmonth
 proj   = ccrs.PlateCarree()
 bbplot = [-80, 0, 35, 75]
 
@@ -226,15 +269,27 @@ ax.set_title("Bounding Box: %s" % (bbstr), fontsize=18,y=1.05)
 outname = figpath + "Bounding_Box_%s.png" % (bbfn)
 plt.savefig(outname, dpi=150, bbox_inches='tight', transparent=True)
 
+#%% Plot the actual anomaly timeseries
+
+fig,ax = plt.subplots(1,1,figsize=(8,3.5),
+                      constrained_layout=True)
 
 
+times  = spg_anom[1].time
+
+for nn in range(ndatasets-2):
+    ts = spg_anom[nn]
+    ax.plot(ts.time,ts,label=dset_names[nn],c=cols[nn],ls=els[nn],lw=2)
+ax.legend(ncol=4)
+
+ax.set_xlim([times[0],times[-1]])
+
+outname = figpath + "SPG_Timeseries_%s.png" % (bbfn)
+plt.savefig(outname, dpi=150, bbox_inches='tight', transparent=True)
 
 #%% NOTE OLD SCRIPT BELOW =-=========
 
-
-
 #%% Do Calculation for OISST --------------------------------------------------
-
 
 bbox_yeager = [-50,-10,50,60] # Original
 bbox_yeager = [-50,-10,50,55] # Shift South
@@ -255,11 +310,6 @@ spg_ts      = spg_ts.rename("OISST_SST")
 outname     = outpath + "OISST_SPG_Box_SST.nc"
 
 spg_ts.to_netcdf(outname)
-
-
-
-
-
 
 #%% Compute the autocorrelation function
 
