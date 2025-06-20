f38c18a5ece5f2ad64e5568ab28813f293a32cf5
Glenn Liu <glenn.y.liu@gmail.com>
2025-06-20 16:43:34 -0400

    2025.06, try to further generalize
    Saving before changes

--- Main/calc_hff_general.py
+++ Main/calc_hff_general.py
@@ -36,41 +36,45 @@ import scipy as sp
 #%% Import Modules (report with hfutils)
 
 # stormtrack
-amvpath = "/home/glliu/00_Scripts/01_Projects/00_Commons/" # amv module
-#amvpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/03_Scripts/" # amv module
+#amvpath = "/home/glliu/00_Scripts/01_Projects/00_Commons/" # amv module
+amvpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/03_Scripts/" # amv module
 sys.path.append(amvpath)
 import amv.proc as hf
+
+
+# Set Path and options
+#datpath           = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/01_hfdamping/output/"
+#datpath           = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/data/NATL_proc_obs/" 
+#datpath           = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/data/NATL_proc_obs/"
+datpath            = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/01_hfdamping/01_Data/"
 
 # -------------
 #%% User Edits
 # -------------
 
-# Set Path and options
-#datpath           = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/01_hfdamping/output/"
-#datpath           = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/data/NATL_proc_obs/" 
-datpath           = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/data/NATL_proc_obs/"
+
 overwrite         = True # Set to True to ovewrite
 debug             = True # Set to true for debugging plots
 
 # Indicate Time Crop (for input)
 croptime          = True
-tstart            =  '1979-01-01'#'1920-01-01'#'0200-01-01' # "2006-01-01" # 
-tend              =  '2021-12-31'#'2005-12-31'#'2000-12-31' #"2101-01-01" # 
+tstart            =  '1979-01-01' #'1920-01-01'#'0200-01-01' # "2006-01-01" # 
+tend              =  '2024-12-31' #'2021-12-31'#'2005-12-31'#'2000-12-31' #"2101-01-01" # 
 timestr           =  '%sto%s'  % (tstart[:4],tend[:4]) # ex. 0000to2000
 
 # Select time crop (for the estimate)
 croptime_estimate = False # Cut time right before estimating the heat flux feedback
-tcrop_start       = '1979-01-01'#"1970-01-01"#'1920-01-01' '2070-01-01'#
-tcrop_end         = '2021-12-31'#"1999-12-31"#'1970-01-01' '2099-12-31'#
+tcrop_start       = '1979-01-01' #"1970-01-01"#'1920-01-01' '2070-01-01'#
+tcrop_end         = '2024-12-31' #'2021-12-31'#"1999-12-31"#'1970-01-01' '2099-12-31'#
 tcrop_fname       = ""
 if croptime_estimate:
     tcrop_fname     = "_%sto%s" % (tcrop_start.replace('-',''),tcrop_end.replace('-',''))
-    
+
 # Indicate bbox crop information
 bbox_name           = "NAtl" # "Global"#
 
 # Variables and Dataset Name
-vnames_in           = ['sst','thflx'] #['TS','LHFLX'] # ["qnet","fsns","flns","lhflx","shflx"] #"TS" for historical data
+vnames_in           = ['sst','qnet']#['sst','thflx'] #['TS','LHFLX'] # ["qnet","fsns","flns","lhflx","shflx"] #"TS" for historical data
 dataset_names       = ["ERA5","ERA5"]#['OISST',"ERA5_RegridOISST"] #'cesm1_htr_5degbilinear'#'cesm2_pic'#'rcp85'
 # Note: Currently hard coded (unfortunately) to read the first dataset name as sst, second as flx
 #hflx_name           = "ERA" # set to None if dataset_name is same as hflx_name
@@ -85,6 +89,7 @@ tname               = 'time'
 # For these datasets, loop for each ensemble member...
 lens_datasets     = ['htr','rcp85','gfdl_esm2m_lens','csiro_mk36_lens','canesm2_lens']
 
+#%%
 # Determine number of ensemble members
 dataset_name = dataset_names[0]
 if dataset_name == 'rcp85':
